60f55eab5c12dc294f22ab6acc1169e873e5fcd1
Martijn <martijn@melchers.nu>
2020-03-30 13:21:59 +0200

    Streaming working, basic socket working too
    Signed-off-by: Martijn <martijn@melchers.nu>

--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,183 @@
-import express, {Request, Response, NextFunction, Express} from "express";
+import express, {Express, NextFunction, Request, Response} from "express";
 import WebTorrent, {Torrent, TorrentFile} from "webtorrent";
-import mongoose, {Connection, Mongoose} from 'mongoose';
+import mongoose, {Connection} from 'mongoose';
 import mime from 'mime';
 import rangeParser from 'range-parser';
-import RangeParser from "range-parser";
 import pump from 'pump';
+import SocketIO from "socket.io";
 
 const app: Express = express();
+const port = 5000;
+
+const videoTypes: Array<string> = [
+    "mp4",
+    "mkv"
+];
 
 mongoose.connect("mongodb://localhost/test", {useUnifiedTopology: true, useNewUrlParser: true});
 
 
 const db: Connection = mongoose.connection;
-const port = 5000;
+
 let file: TorrentFile;
 const client = new WebTorrent();
-const torrentString = "magnet:?xt=urn:btih:E156CE70FEE6464549FEF653C8468AECB3E7E9F1&dn=Black+Panther+%282018%29+%5BBluRay%5D+%5B1080p%5D+%5BYTS%5D+%5BYIFY%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce";
+const torrentString = "magnet:?xt=urn:btih:B6E82665EF588BB6574DB1F9780A0279274F407D&dn=Aquaman+%282018%29+%5BWEBRip%5D+%5B1080p%5D+%5BYTS%5D+%5BYIFY%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce";
 
+const server = app.listen(port, () => {
+
+    console.log(`Server started at http://localhost:${port}`);
+});
+
+const io = require("socket.io")(server);
 
 db.once("open", () => {
     console.log("Boom! We're connected")
 });
 
-client.add(torrentString, (torrent: Torrent) => {
-    var mp4 = torrent.files.find(x => x.name.endsWith(".mp4"));
-
-    if (mp4)
-        file = mp4;
+client.on("torrent", (a) => {
+    console.log(`Torrent added! ${a.name}`);
+    io.emit("ready");
 });
 
+client.add(torrentString, (torrent: Torrent) => {
+
+    torrent.on("done", () => {
+        io.emit("done");
+
+    });
+
+    torrent.on("download", (bytes) => {
+        io.emit("progress", {
+            progress: torrent.progress,
+            speed: torrent.downloadSpeed,
+            peers: torrent.numPeers
+        });
+    });
+
+
+    const videoFile = torrent.files.find(x => videoTypes.includes(x.name.substring(x.name.lastIndexOf(".") + 1)));
+
+    if (videoFile)
+        file = videoFile;
+});
+
+
+io.on("connection", (socket: SocketIO.Socket) => {
+    console.log(`User connected! ${socket.id}`);
+});
 
 app.get("/", (req: Request, res: Response<{ running: boolean }>, next: NextFunction) => {
     res.json({running: true});
 });
 
-app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+
+app.get("/stream", (req: Request, res: Response, next: NextFunction) => {
+
+    if(file == null) {
+        res.statusCode = 200;
+        res.end();
+        return;
+    }
+
     res.statusCode = 200;
     res.setHeader('Content-Type', mime.getType(file.name) || 'application/octet-stream');
+
+    // Support range-requests
     res.setHeader('Accept-Ranges', 'bytes');
 
+    // Set name of file (for "Save Page As..." dialog)
     res.setHeader(
         'Content-Disposition',
         `inline; filename*=UTF-8''${encodeRFC5987(file.name)}`
     );
 
-
     // Support DLNA streaming
-    res.setHeader('transferMode.dlna.org', 'Streaming')
+    res.setHeader('transferMode.dlna.org', 'Streaming');
     res.setHeader(
         'contentFeatures.dlna.org',
         'DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=01700000000000000000000000000000'
     );
 
-    let range: RangeParser.Ranges | -1 | -2 | null = rangeParser(file.length, req.headers.range || '');
+    // `rangeParser` returns an array of ranges, or an error code (number) if
+    // there was an error parsing the range.
+    let range = rangeParser(file.length, req.headers.range || '');
 
-    function isValidRange(range: RangeParser.Ranges | -1 | -2 | null) {
-        return range != -1 && range != -2 && range != null;
-    }
-
-    if (Array.isArray(range) && isValidRange(range)) {
+    if (Array.isArray(range)) {
         res.statusCode = 206; // indicates that range-request was understood
 
+        // @ts-ignore
         // no support for multi-range request, just use the first range
-        const firstRange = range[0];
+        range = range[0];
 
+        // @ts-ignore
+
+        console.log(`START: ${range.start} END: ${range.end}, length: ${file.length}`);
 
         res.setHeader(
             'Content-Range',
-            `bytes ${firstRange.start}-${firstRange.end}/${file.length}`
+            // @ts-ignore
+            `bytes ${range.start}-${range.end}/${file.length}`
         );
-        res.setHeader('Content-Length', firstRange.end - firstRange.start + 1)
+        // @ts-ignore
+
+        res.setHeader('Content-Length', range.end - range.start + 1)
     } else {
+        // @ts-ignore
+
         range = null;
         res.setHeader('Content-Length', file.length)
     }
 
     if (req.method === 'HEAD') {
-        return res.end();
+        return res.end()
     }
-
-    if(!isValidRange(range))
-        return res.end();
-
     // @ts-ignore
+
     pump(file.createReadStream(range), res)
 });
 
-app.listen(port, () => {
+app.get("/stream-v2", (req: Request, res: Response) => {
 
-    console.log(`Server started at http://localhost:${port}`);
+    // fs.stat(file, function (err, stats) {
+    //     if (err) {
+    //         if (err.code === 'ENOENT') {
+    //             // 404 Error if file not found
+    //             return res.sendStatus(404);
+    //         }
+    //         res.end(err);
+    //     }
+    //     var range = req.headers.range;
+    //     if (!range) {
+    //         // 416 Wrong range
+    //         return res.sendStatus(416);
+    //     }
+    //     var positions = range.replace(/bytes=/, "").split("-");
+    //     var start = parseInt(positions[0], 10);
+    //     var total = stats.size;
+    //     var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    //     var chunksize = (end - start) + 1;
+    //
+    //     res.writeHead(206, {
+    //         "Content-Range": "bytes " + start + "-" + end + "/" + total,
+    //         "Accept-Ranges": "bytes",
+    //         "Content-Length": chunksize,
+    //         "Content-Type": "video/mp4"
+    //     });
+    //
+    //     var stream = fs.createReadStream(file, {start: start, end: end})
+    //         .on("open", function () {
+    //             stream.pipe(res);
+    //         }).on("error", function (err) {
+    //             res.end(err);
+    //         });
+    // });
 });
+
+app.get("/finish", (req: Request, res: Response) => {
+    client.remove(torrentString);
+    res.send({deleted: true});
+});
+
 
 function encodeRFC5987(str: string) {
     return encodeURIComponent(str)
