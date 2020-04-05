7dc4d79251c17ec5e0b6ff0bf8b389ad15bb4507
SaschaMendel <saschamendel2016@gmail.com>
2020-04-05 15:26:33 +0200

    Switch queue

--- src/controllers/room.controller.ts
+++ src/controllers/room.controller.ts
@@ -10,6 +10,7 @@ import StreamController from './stream.controller';
 import parseTorrent from 'parse-torrent';
 import SocketController from './socket.controller';
 import {isRoomAdmin} from '../middleware/is-room-admin';
+import {isRoomManager} from '../middleware/is-room- manager';
 
 class RoomController {
 	public path = '/rooms';
@@ -43,8 +44,9 @@ class RoomController {
 		this.router.put(`${this.path}/:roomId/users/:email`, auth.required, isRoomAdmin,  (req, res) => this.setRole(req,res));
 
 		// Queue Routes
-		this.router.post(`${this.path}/:roomId/queue`, auth.required, inRoom, (req, res) => this.addToQueue(req, res));
-		this.router.delete(`${this.path}/:roomId/queue/:queueItemPos`, auth.required, inRoom, (req, res) => this.removeFromQueue(req, res));
+		this.router.post(`${this.path}/:roomId/queue`, auth.required, isRoomManager, (req, res) => this.addToQueue(req, res));
+		this.router.put(`${this.path}/:roomId/queue`,auth.required, isRoomManager, (req, res) => this.moveQueue(req, res));
+		this.router.delete(`${this.path}/:roomId/queue/:queueItemPos`, auth.required, isRoomManager, (req, res) => this.removeFromQueue(req, res));
 	}
 
 
@@ -243,6 +245,46 @@ class RoomController {
 			});
 	};
 
+	private async moveQueue(req: Request, res: Response){
+        if (!req.params.roomId) res.sendStatus(400);
+
+        const oldPos = req.body.oldPos;
+        const newPos = req.body.newPos;
+        if(!oldPos || !newPos)
+            return res.sendStatus(400);
+
+
+        let room = await Rooms.findOne({Id: req.params.roomId})
+            .populate({ path: 'Users.User', model: 'User' })
+            .populate({ path: 'Users.Role', model: 'Role' }).exec();
+
+        if(!room)
+            return res.sendStatus(404);
+
+
+        let oldIndex = -1;
+        let newIndex = -1;
+
+        room.Queue.forEach((item, index) => {
+           if(item.Position == oldPos){
+               oldIndex = index;
+           }
+           if(item.Position == newPos){
+               newIndex = index;
+           }
+        });
+
+        room.Queue[oldIndex].Position = newPos;
+        room.Queue[newIndex].Position = oldPos;
+
+        room = await Rooms.findOneAndUpdate({ Id: room.Id}, {Queue: room.Queue}, {new: true}).exec();
+        if(!room)
+            return res.sendStatus(500);
+
+        return res.json(room.toJSON());
+    }
+
+
 	private async getUser(req: Request, res: Response) {
 		if (!req.params.roomId || !req.params.email) res.sendStatus(400);
 
@@ -335,7 +377,11 @@ class RoomController {
 
         const email = req.params.email;
         const roomId = req.params.roomId;
-        let room = await Rooms.findOne({Id : roomId}).populate({path: 'Users.Role', model: 'Role'}).populate({path: 'Users.User', model: 'User'}).exec();
+        let room = await Rooms.findOne({Id : roomId})
+            .populate({path: 'Users.Role', model: 'Role'})
+            .populate({path: 'Users.User', model: 'User'})
+            .exec();
+
         const role = await Roles.findOne({PermissionLevel: permissionLevel}).exec();
 
         if(!room || !role)
